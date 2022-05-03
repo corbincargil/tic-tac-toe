af58374c436f23c0653a1166c20b4095e982ddce
corbincargil <corbin.cargil@gmail.com>
2022-05-03 17:34:42 -0500

    added objects for gameboard, players, and game controls. Added method in the gameboard object that creates and displays the board.

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
-//gameboard object (module pattern)
-const gameboard = (() => {
-    const board = [];
-    const _createGameboard = () => {
-        for (let i = 0; i < 9; i++) {
-            board += array[i];
-        }
-    }
-
-})();
+//                                  DOM Elements
+const boardContainer = document.querySelector('#gameboard-container');
+const controlsContainer = document.querySelector('#controls-container');
 
 //player object (factory function)
-const player = (name,symbol,) => {
+const player = (name,symbol) => {
     name;
     symbol;
-    selection;
+    let selection;
     return {name, symbol, selection} 
 }
 
 const playerOne = player('Player 1','x');
+const playerTwo = player('Player 2', 'o');
 
-console.log(playerOne.name);
+//gameboard object (module pattern)
+const gameboard = (() => {
+    const board = [];
+    (() => {
+        for (let i = 0; i < 9; i++) {board.push(i);}
+        return board;
+    })();
+    const updateBoard = (player, cellNumber) => {
+        symbol = player.symbol;
+        const cell = boardContainer.getElementById(cellNumber);
+    }
+    return {board}
+})();
+
+
 
 //game controls (module pattern)
+
+//                                  Gameplay
+
+//add board to the document
+gameboard.board.forEach((cell) => {
+    newDiv = document.createElement('div');
+    newDiv.classList.add('board-cell');
+    newDiv.setAttribute('id',`${gameboard.board[cell]}`);
+    boardContainer.appendChild(newDiv);
+});
