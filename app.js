9f0f543e677a1cb1b8a09fc77d4e433ddfb6313b
corbincargil <corbin.cargil@gmail.com>
2022-05-03 19:36:28 -0500

    added playerTurn method in the gameBoard object

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ const boardContainer = document.querySelector('#gameboard-container');
 const controlsContainer = document.querySelector('#controls-container');
 
 //player object (factory function)
-const player = (name,symbol) => {
+const player = (name) => {
     name;
-    symbol;
-    let selection;
-    return {name, symbol, selection} 
+    return {name} 
 }
 
-const playerOne = player('Player 1','x');
-const playerTwo = player('Player 2', 'o');
+const playerOne = player('Player 1');
+const playerTwo = player('Player 2');
 
 //gameboard object (module pattern)
 const gameboard = (() => {
@@ -20,16 +18,51 @@ const gameboard = (() => {
         for (let i = 0; i < 9; i++) {board.push(i);}
         return board;
     })();
-    const updateBoard = (player, cellNumber) => {
-        symbol = player.symbol;
-        const cell = boardContainer.getElementById(cellNumber);
+
+    const updateBoard = (cellClicked) => {
+        let symbol;
+        if (boardContainer.classList.contains('player-1')) {symbol = 'x'}
+        else {symbol = 'o'}
+        boardContainer.classList.toggle('player-1')
+        boardContainer.classList.toggle('player-2')
+        const cellNumber = cellClicked.target.id;
+        cell = document.getElementById(cellNumber);
+        cell.textContent = symbol;
     }
-    return {board}
+
+    const checkForWinner = (cellOne, cellTwo, cellThree) => {
+        let symbol = 'x';
+        let cellNumbers = [cellOne, cellTwo, cellThree];
+        let i = 0;
+        cellNumbers.forEach(cell => {
+            const cellContent = board[cell].textContent;
+            if (cellContent == symbol) {i++}
+        });
+        if (i === 3) {
+            console.log('Player 1 is the winner!');
+         } else {}
+        
+         symbol = 'o';
+         i = 0;
+         cellNumbers.forEach(cell => {
+            const cellContent = board[cell].textContent;
+            if (cellContent == symbol) {i++}
+        });
+        if (i === 3) {
+            console.log('Player 2 is the winner!');
+         } else {}
+    }
+
+    return {board, updateBoard, checkForWinner}
 })();
 
 
 
 //game controls (module pattern)
+
+const gameControls = (() => {
+    
+})();
 
 //                                  Gameplay
 
@@ -38,5 +71,8 @@ gameboard.board.forEach((cell) => {
     newDiv = document.createElement('div');
     newDiv.classList.add('board-cell');
     newDiv.setAttribute('id',`${gameboard.board[cell]}`);
+    newDiv.addEventListener('click', gameboard.updateBoard);
+    newDiv.addEventListener('click',gameboard.checkForWinner(0,1,2))
     boardContainer.appendChild(newDiv);
+    
 });
