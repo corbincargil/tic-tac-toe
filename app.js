9cc51f49517da8ef593150d8ea6ddd5ba011f615
corbincargil <corbin.cargil@gmail.com>
2022-05-05 15:13:53 -0500

    udpted user interface. added improved game logic.

--- app.js
+++ app.js
@@ -1,15 +1,67 @@
 //                                  DOM Elements
 const boardContainer = document.querySelector('#gameboard-container');
 const controlsContainer = document.querySelector('#controls-container');
+const playerOneContainer = document.querySelector('#player-one-container');
+const playerTwoContainer = document.querySelector('#player-two-container');
+const playerOnePic = document.querySelector('#pic-one-container');
+const playerTwoPic = document.querySelector('#pic-two-container');
+const playerOneWinner = playerOneContainer.querySelector('.win-status');
+const playerTwoWinner = playerTwoContainer.querySelector('.win-status');
+const resetButton = document.getElementById('reset-btn');
+
+
+
 
 //player object (factory function)
-const player = (name) => {
-    name;
-    return {name} 
+const player = (name, number) => {
+    name; 
+    
+    const toggleTurnStatus = (number) => {
+        if (number == 1) {
+            playerOneContainer.classList.toggle('your-turn');
+            playerOneStatus = playerOneContainer.querySelector('.turn-status');
+            playerOneStatus.classList.toggle('your-turn');
+            playerOneStatus.classList.toggle('not-your-turn');
+            playerOnePic.classList.toggle('your-turn');
+            playerOnePic.classList.toggle('not-your-turn');
+        } else if (number == 2) {
+            playerTwoContainer.classList.toggle('your-turn');
+            playerTwoStatus = playerTwoContainer.querySelector('.turn-status');
+            playerTwoStatus.classList.toggle('your-turn');
+            playerTwoStatus.classList.toggle('not-your-turn');
+            playerTwoPic.classList.toggle('your-turn');
+            playerTwoPic.classList.toggle('not-your-turn');
+        } else {console.log('No player # input')}
+
+    }
+
+    const toggleWinner = (number) => {
+        if (number == 1) {
+            playerOneContainer.classList.toggle('winner');
+            playerOneWinner.classList.toggle('winner');
+            playerOneWinner.classList.toggle('off');
+        } else if (number == 2) {
+            playerTwoContainer.classList.toggle('winner');
+            playerTwoWinner.classList.toggle('winner');
+            playerTwoWinner.classList.toggle('off');
+
+
+        } else {console.log('no player # for winner')}
+    }
+
+    const toggleLoser = (number) => {
+        if (number == 1) {
+            playerOneContainer.classList.toggle('loser');
+        } else if (number == 2) {
+            playerTwoContainer.classList.toggle('loser');
+        } else {console.log('no player # for loser')}
+    }
+
+    return {name, toggleTurnStatus, toggleWinner, toggleLoser}
 }
 
-const playerOne = player('Player 1');
-const playerTwo = player('Player 2');
+const playerOne = player('Player 1',1);
+const playerTwo = player('Player 2',2);
 
 //gameboard object (module pattern)
 const gameboard = (() => {
@@ -30,6 +82,17 @@ const gameboard = (() => {
         cell.textContent = symbol;
     }
 
+    const gameFinish = () => {
+        board.forEach(cell => {
+            cell = document.getElementById(cell);
+            cell.removeEventListener('click', gameboard.updateBoard, {once:true});
+            cell.removeEventListener('click', gameboard.runAllChecks);
+            playerOneContainer.querySelector('.turn-status').classList.add('off');
+            playerTwoContainer.querySelector('.turn-status').classList.add('off');
+        });
+
+    }
+
     const checkForWinner = (cellOne, cellTwo, cellThree) => {
         let symbol = 'x';
         let cellNumbers = [cellOne, cellTwo, cellThree];
@@ -37,20 +100,26 @@ const gameboard = (() => {
         cellNumbers.forEach(cell => {
             const cellContent = document.getElementById(cell).textContent;
             if (cellContent === symbol) {i++}
-        });
+        })
         if (i === 3) {
-            console.log('Player 1 is the winner!');
-         } else {}
+            playerOne.toggleWinner(1);
+            playerTwo.toggleLoser(2);
+            // playerOneContainer.querySelector('.player-status').textContent = 'WINNER!'
+            gameFinish();
+        }
         
-         symbol = 'o';
-         i = 0;
-         cellNumbers.forEach(cell => {
+        symbol = 'o';
+        i = 0;
+        cellNumbers.forEach(cell => {
             const cellContent = document.getElementById(cell).textContent;
             if (cellContent == symbol) {i++}
-        });
+            })
         if (i === 3) {
-            console.log('Player 2 is the winner!');
-         } else {}
+            playerOne.toggleLoser(1);
+            playerTwo.toggleWinner(2);
+
+            gameFinish();
+        }
     }
 
     const runAllChecks = () => {
@@ -77,8 +146,9 @@ gameboard.board.forEach((cell) => {
     newDiv = document.createElement('div');
     newDiv.classList.add('board-cell');
     newDiv.setAttribute('id',`${gameboard.board[cell]}`);
-    newDiv.addEventListener('click', gameboard.updateBoard);
-    newDiv.addEventListener('click',gameboard.runAllChecks)
+    newDiv.addEventListener('click', gameboard.updateBoard, {once:true});
+    newDiv.addEventListener('click', gameboard.runAllChecks);
+    newDiv.addEventListener('click', () => {playerOne.toggleTurnStatus(1)},{once:true});
+    newDiv.addEventListener('click', () => {playerTwo.toggleTurnStatus(2)}, {once:true});
     boardContainer.appendChild(newDiv);
-    
 });
